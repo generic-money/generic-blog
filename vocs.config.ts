fa39c3e6e1f5f2f3608e3e9d7d9c30137c4e5507
ashhanai <ashhab.naim@gmail.com>
2026-01-14 14:45:13 +0100

    feat: update og image

--- vocs.config.ts
+++ vocs.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
     dark: '/img/logo-white.svg',
   },
   title: 'Generic Blog',
-  ogImageUrl: '/img/og-image.png',
+  ogImageUrl: 'https://generic-blog-gamma.vercel.app/img/og-image.png',
 })
